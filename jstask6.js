e497d3429503371ab34aa5cb7f7afae8064aa737
TejaswinireddyDev <tejaswinireddythammara246@gmail.com>
2024-10-24 20:37:26 +0530

    conditional statements

---
+++ jstask6.js
@@ -0,0 +1,61 @@
+// 1__ Traffic light system 
+var light="Blue";
+if (light=="Red"){
+    console.log("Stop")
+}
+else if (light=="Yellow"){
+    console.log("Get ready to move")
+}
+else if (light=="Green"){
+    console.log("Go")                
+}
+else{
+    console.log("Invalid input")
+}
+// Output:-- Invalid input
+
+
+// 2__ Weather description 
+var temp=25;
+if (temp==0){
+    console.log("it's freezing cold ")
+}
+else if(temp==15){
+    console.log("it's a cool day")
+}
+else if(temp==25){
+    console.log("it's a pleasant day")
+}
+else{
+    console.log("unknown weather")
+}
+// Output:-- it's a pleasant day
+
+
+// 3___Day of the week
+var num=6;
+if(num==1){
+    console.log("Today is Monday")
+}
+else if(num==2){
+    console.log("Today is Tuesday")
+}
+else if(num==3){
+    console.log("Today is Wednesday")
+}
+else if(num==4){
+    console.log("Today is Thursday")
+}
+else if(num==5){
+    console.log("Today is Friday")
+}
+else if(num==6){
+    console.log("Today is Saturday")
+}
+else if(num==7){
+    console.log("Today is Sunday")
+}
+else{
+    console.log("Invalid day number")
+}
+// Output:-- Today is Saturday
